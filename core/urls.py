1d746d1969ef9a4f0299e7ef33c4171cce3b49b4
kspr <kspr9@pm.me>
2023-11-04 11:59:45 +0200

    added simple login page and related url routing

--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 from tokenshare.views import SpaView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", SpaView.as_view(), name="spa"),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", SpaView.as_view(), name="tokenshare"),
 ]
--- tokenshare/views.py
+++ tokenshare/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class SpaView(TemplateView):
+class SpaView(LoginRequiredMixin, TemplateView):
     template_name = "tokenshare/index.html"
